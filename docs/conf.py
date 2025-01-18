5118fd3be7298ff2750c0d6014c935b1ec40c161
Michael Makukha <m.makukha@gmail.com>
2025-01-18 15:25:27 +0200

    Bump version, update history and changelog (#23)

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath('../src'))
 project = 'importloc'
 copyright = '2025, Michael Makukha'
 author = 'Michael Makukha'
-release = '0.1.1'
+release = '0.2.0'
 
 # general
 templates_path = ['_templates']
